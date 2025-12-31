85017f30e6326b730e21b57e637a52d4f951eb79
COPA100 <colinpark100@gmail.com>
2025-12-31 16:53:47 -0500

    example csv files and trying out reading from them

--- backend/csv_reader.py
+++ backend/csv_reader.py
@@ -2,8 +2,13 @@ import numpy as np
 import pandas as pd
 
 # file goes here
-CSV_FILE = ""
+CSV_FILE = "example_csv\ex1.csv"
 
 # change to dataframe
-csvFile = pd.read_csv(CSV_FILE)
+CSV = pd.read_csv(CSV_FILE, skiprows=2, skipfooter=2)
 
+print(CSV.columns)
+
+investments = CSV[["Symbol", "Qty (Quantity)"]]
+
+print(investments)
